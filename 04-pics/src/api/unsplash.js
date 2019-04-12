dc229478daae11d5cd6a9f344f507e7409cec317
goldensky09 <vikalp.parashar@gmail.com>
2019-04-12 16:25:52 +0530

    04 pics complete

---
+++ 04-pics/src/api/unsplash.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+export default axios.create({
+    baseURL: "https://api.unsplash.com",
+    headers: {
+        Authorization: 'Client-ID 9e20df49fbd4617d5ba3558ffce77926a9c4ba34fd9cc4c92579fdcfadb739f3'
+    }
+});
